618f1380f2f098ee7b62f93194d015971f4ac401
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-03-27 13:41:32 +1100

    add contact

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import json
 import os.path
+import random
 import urllib.parse
 import requests
+
+
+def contact(person):
+    name = person["name"].lower().split()
+
+    random.seed(person["name"])  # so that the same name -> same email
+    provider = random.choice(["@hotmail.com", "@gmail.com", "@outlook.com"])
+    number = random.randint(1, 99)
+    if len(name) == 1:
+        firstname = name[0]
+        return f"{firstname}{number}{provider}"
+    elif len(name) == 2:
+        firstname, lastname = name
+        if random.randint(0, 1):
+            return f"{lastname[0]}{firstname}{number}{provider}"
+        return f"{firstname}{lastname}{number}{provider}"
+
+    return f"{''.join(name)}{number}{provider}"
 
 
 def json_from_path(uri):
@@ -10,6 +29,7 @@ def json_from_path(uri):
 
 def cache_available():
     return os.path.exists("cache.json")
+
 
 def fetch_people(uri="https://swapi.dev/api/people/"):
     if cache_available():
@@ -50,7 +70,7 @@ print(f"{len(people)} people meet your requirements")
 print('-' * 80)
 
 for index, person in enumerate(people):
-    print(f"[{index + 1}] {person['name']}")
+    print(f"[{index + 1}] {person['name']} | Contact: {contact(person)}")
     print("Experience:")
     try:
         vehicles = [json_from_path(uri) for uri in person['vehicles']]
